da75d260a8836d5599cfbc554ec88f212ac6dece
rainbow <562783546@qq.com>
2025-03-12 22:10:45 +0800

    dws 12.4 用户域用户登录各窗口汇总表

---
+++ realtime-dws/realtime-dws-user-user-login-window/src/main/java/dws/app/DwsUserUserLoginWindow.java
@@ -0,0 +1,154 @@
+package dws.app;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.AllWindowedStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.windowing.AllWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+import rainbow.realtime.common.Base.BaseApp;
+import rainbow.realtime.common.bean.UserLoginBean;
+import rainbow.realtime.common.constant.Constant;
+import rainbow.realtime.common.function.BeanToJsonStrMapFunction;
+import rainbow.realtime.common.util.DateFormatUtil;
+import rainbow.realtime.common.util.FlinkSinkUtil;
+
+/**
+ * @author rainbow
+ * @time 2025-03-12 20:28
+ * @description ...
+ */
+public class DwsUserUserLoginWindow extends BaseApp {
+    public static void main(String[] args) {
+        new DwsUserUserLoginWindow().start(
+                10024,
+                4,
+                "dws_user_user_login_window",
+                Constant.TOPIC_DWD_TRAFFIC_PAGE
+        );
+    }
+
+    @Override
+    public void handle(StreamExecutionEnvironment env, DataStreamSource<String> kafkaStrDS) {
+        //从页面日志事实表读取数据
+        SingleOutputStreamOperator<JSONObject> jsonObjDS = kafkaStrDS.map(JSON::parseObject);
+        //过滤出登录行为
+        SingleOutputStreamOperator<JSONObject> filterDS = jsonObjDS.filter(
+                new FilterFunction<JSONObject>() {
+                    @Override
+                    public boolean filter(JSONObject jsonObj) throws Exception {
+                        String uId = jsonObj.getJSONObject("common").getString("uid");
+                        String lastPageId = jsonObj.getJSONObject("page").getString("last_page_id");
+                        return StringUtils.isNotEmpty(uId)
+                                && ("login".equals(lastPageId) || StringUtils.isEmpty(lastPageId));
+                    }
+                }
+        );
+        //指定watermark
+        SingleOutputStreamOperator<JSONObject> withWatermarkDS = filterDS.assignTimestampsAndWatermarks(
+                WatermarkStrategy
+                        .<JSONObject>forMonotonousTimestamps()
+                        .withTimestampAssigner(
+                                new SerializableTimestampAssigner<JSONObject>() {
+                                    @Override
+                                    public long extractTimestamp(JSONObject element, long recordTimestamp) {
+                                        return element.getLong("ts");
+                                    }
+                                }
+                        )
+        );
+        //按照uid进行分组
+        KeyedStream<JSONObject, String> keyedDS = withWatermarkDS.keyBy(jsonObj -> jsonObj.getJSONObject("common").getString("uid"));
+        //判断是否为独立用户和回流用户
+        SingleOutputStreamOperator<UserLoginBean> beanDS = keyedDS.process(
+                new KeyedProcessFunction<String, JSONObject, UserLoginBean>() {
+                    private ValueState<String> lastLoginDateState;
+
+                    @Override
+                    public void open(Configuration parameters) throws Exception {
+                        ValueStateDescriptor<String> valueStateDescriptor
+                                = new ValueStateDescriptor<>("lastLoginDateState", String.class);
+                        //不用设置失效时间
+                        lastLoginDateState = getRuntimeContext().getState(valueStateDescriptor);
+                    }
+
+                    @Override
+                    public void processElement(JSONObject value, KeyedProcessFunction<String, JSONObject, UserLoginBean>.Context ctx, Collector<UserLoginBean> out) throws Exception {
+                        //获取上次登录日期
+                        String lastLoginDate = lastLoginDateState.value();
+                        //获取当前登录的日期
+                        Long ts = value.getLong("ts");
+                        String curLoginDate = DateFormatUtil.tsToDate(ts);
+
+                        long uuCt = 0L;
+                        long backCt = 0L;
+
+                        if (StringUtils.isNotEmpty(lastLoginDate)) {
+                            if (!lastLoginDate.equals(curLoginDate)) {
+                                uuCt = 1L;
+                                lastLoginDateState.update(curLoginDate);
+                                long day = (ts - DateFormatUtil.dateToTs(lastLoginDate)) / 1000 / 60 / 60 / 24;
+                                if (day >= 8) {
+                                    backCt = 1L;
+                                }
+                            }
+                        } else {
+                            uuCt = 1L;
+                            lastLoginDateState.update(curLoginDate);
+                        }
+
+                        if (uuCt != 0L || backCt != 0L) {
+                            out.collect(new UserLoginBean("", "", "", backCt, uuCt, ts));
+                        }
+
+                    }
+                }
+        );
+        //开窗
+        AllWindowedStream<UserLoginBean, TimeWindow> windowDS = beanDS.windowAll(TumblingEventTimeWindows.of(Time.seconds(10)));
+
+        //聚合
+        SingleOutputStreamOperator<UserLoginBean> reduceDS = windowDS.reduce(
+                new ReduceFunction<UserLoginBean>() {
+                    @Override
+                    public UserLoginBean reduce(UserLoginBean value1, UserLoginBean value2) throws Exception {
+                        value1.setBackCt(value1.getBackCt() + value2.getBackCt());
+                        value1.setUuCt(value1.getUuCt() + value2.getUuCt());
+                        return value1;
+                    }
+                },
+                new AllWindowFunction<UserLoginBean, UserLoginBean, TimeWindow>() {
+                    @Override
+                    public void apply(TimeWindow window, Iterable<UserLoginBean> values, Collector<UserLoginBean> out) throws Exception {
+                        UserLoginBean userLoginBean = values.iterator().next();
+                        String stt = DateFormatUtil.tsToDateTime(window.getStart());
+                        String edt = DateFormatUtil.tsToDateTime(window.getEnd());
+                        String curDate = DateFormatUtil.tsToDate(window.getStart());
+                        userLoginBean.setStt(stt);
+                        userLoginBean.setEdt(edt);
+                        userLoginBean.setCurDate(curDate);
+                        out.collect(userLoginBean);
+                    }
+                }
+        );
+        //insert into doris
+        reduceDS.print();
+        reduceDS.map(new BeanToJsonStrMapFunction<>())
+                .sinkTo(FlinkSinkUtil.getDorisSink("dws_user_user_login_window"));
+    }
+}
